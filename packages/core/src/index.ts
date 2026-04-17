2c6e78d9b80c8723ebd9cfe4ddc0358ed83f5f34
unvalley <kirohi.code@gmail.com>
2026-04-17 18:37:36 +0900

    feat(core): export public API

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,14 @@
-export {}
+export { createCommand } from './store'
+export { commandScore } from './score'
+export { normalize } from './normalize'
+export type {
+  CommandOptions,
+  CommandState,
+  CommandStore,
+  FilterFn,
+  GroupData,
+  GroupInput,
+  ItemData,
+  ItemInput,
+  PointerSelectionMode,
+} from './types'
